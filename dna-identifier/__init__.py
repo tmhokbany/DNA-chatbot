5c950157f5e1ada27b45a21a0936dab3ce77692d
Tala <tmhokbany@gmail.com>
2026-09-25 01:15:43 +0300

    Add new imports and update __all__ in __init__.py

--- dna-identifier/__init__.py
+++ dna-identifier/__init__.py
@@ -1,6 +1,8 @@
 from .sequence_utils import clean_sequence, is_valid_dna, sequence_stats
 from .features import KmerFeaturizer
 from .model import SequenceIdentifier
+from .locate import LocationMatch, locate
+from .reference_data import load_gene_annotations, load_reference_sequences
 
 __all__ = [
     "clean_sequence",
@@ -8,4 +10,8 @@ __all__ = [
     "sequence_stats",
     "KmerFeaturizer",
     "SequenceIdentifier",
+    "LocationMatch",
+    "locate",
+    "load_gene_annotations",
+    "load_reference_sequences",
 ]
